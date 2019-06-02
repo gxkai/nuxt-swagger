83c8ace6b3faa8d79bf29bdda3413d78b85b45f9
guxukai <1378026744@qq.com>
2019-06-02 08:50:39 +0800

    init

---
+++ server/index.js
@@ -0,0 +1,54 @@
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import cors from 'koa-cors';
+
+import errorHandle from './middleware/errorHandle';
+import router from './routes/index';
+
+const serve = require('koa-static');const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
+
+const app = new Koa()
+app
+  .use(cors())
+  .use(serve('.'))
+  .use(bodyParser())
+  .use(errorHandle())
+  .use(router.routes())
+  .use(router.allowedMethods());
+// Import and Set Nuxt.js options
+let config = require('../nuxt.config.js')
+config.dev = !(app.env === 'production')
+
+async function start() {
+  // Instantiate nuxt.js
+  const nuxt = new Nuxt(config)
+
+  const {
+    host = process.env.HOST || '127.0.0.1',
+    port = process.env.PORT || 3000
+  } = nuxt.options.server
+
+  // Build in development
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  } else {
+    await nuxt.ready()
+  }
+
+  app.use(ctx => {
+    ctx.status = 200
+    ctx.respond = false // Bypass Koa's built-in response handling
+    ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
+    nuxt.render(ctx.req, ctx.res)
+  })
+
+  app.listen(port, host)
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  })
+}
+
+start()
